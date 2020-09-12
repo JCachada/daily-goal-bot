dc554d4db5986fc67de686428d42304d11d07201
João Cachada <jmcs.cachada@gmail.com>
2020-09-12 16:04:43 +0100

    feat: adds OAuth flow to app initialization

--- app.js
+++ app.js
@@ -6,15 +6,23 @@ require('dotenv').config();
 
 const jobs = require('./jobs');
 
+const listeners = require('./listeners');
+
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
+  clientId: process.env.SLACK_CLIENT_ID,
+  clientSecret: process.env.SLACK_CLIENT_SECRET,
+  stateSecret: 'my-state-secret',
+  scopes: ['chat:write.public', 'commands', 'chat:write'],
 });
 
 (async () => {
   await app.start(process.env.PORT || 3000);
 
   jobs.setupJobs();
+
+  listeners.setupListeners();
 
   console.log('⚡️ App is running!');
 })();
