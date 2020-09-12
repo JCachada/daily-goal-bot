558ee8b4b3e9772775cab9d9e1b3befec2cd398e
João Cachada <jmcs.cachada@gmail.com>
2020-09-12 12:53:51 +0100

    feat: adds dotenv package to ensure .env files are loaded in production

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const axios = require('axios').default;
 
 require('node-schedule');
 
+require('dotenv').config();
+
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
